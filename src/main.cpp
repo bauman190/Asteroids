fb4fb18570577ea952a0053a38f543cc0024d0a5
Bautista <bauman@hotmail.com>
2023-10-06 10:10:28 -0300

    add open Window

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,23 @@
+#include "raylib.h"
 void main() 
 {
+    const int screenWidth = 800;
+    const int screenHeight = 450;
 
+    InitWindow(screenWidth, screenHeight, "Asteriods");
+
+
+    while (!WindowShouldClose())    
+    {
+        
+        BeginDrawing();
+
+        ClearBackground(BLACK);      
+
+        EndDrawing();
+        
+    }
+
+    
+    CloseWindow();
 }
