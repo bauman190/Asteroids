afeb15e5ed97f9c644d8626d0fbadbc5ca186644
bautista <baumancastignani@hotmail.com>
2024-10-24 00:11:45 -0300

    asteroids only generate on screen borders

--- src/Asteroid.cpp
+++ src/Asteroid.cpp
@@ -7,10 +7,37 @@ extern const int asteroidsAmount = 10;
 
 void inItAsteroid(asteroid asteroids[])
 {
+	int randN;
+
 	for (int  i = 0; i < asteroidsAmount; i++)
 	{
-		asteroids[i].collider.pos.x = static_cast<float>(GetRandomValue(0, GetScreenWidth()));
-		asteroids[i].collider.pos.y = static_cast<float>(GetRandomValue(0, GetScreenHeight()));
+		randN = GetRandomValue(0, 1);
+		if (randN == 0)
+		{
+			randN = GetRandomValue(0, 1);
+			asteroids[i].collider.pos.x = static_cast<float>(GetRandomValue(0, GetScreenWidth()));
+			if (randN == 0)
+			{
+				asteroids[i].collider.pos.y = 0;
+			}
+			else
+			{
+				asteroids[i].collider.pos.y = static_cast<float>(GetScreenHeight());
+			}
+		}
+		else
+		{
+			randN = GetRandomValue(0, 1);
+			if (randN == 0)
+			{
+				asteroids[i].collider.pos.x = 0;
+			}
+			else
+			{
+				asteroids[i].collider.pos.x = static_cast<float>(GetScreenWidth());
+			}
+			asteroids[i].collider.pos.y = static_cast<float>(GetRandomValue(0, GetScreenHeight()));
+		}
 		asteroids[i].collider.radius = 20;
 	}
 	
